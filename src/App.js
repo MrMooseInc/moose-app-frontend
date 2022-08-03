8ca6e4546a7bee853cbeeba67e1bd309fb5313dd
greg <gregorybwest@gmail.com>
2022-08-02 20:46:38 -0700

    displays tasks and details, needs improvement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+// import {useState} from 'react';
+// import axios from 'axios';
 
-function App() {
+// const App = () => {
+//   const [data, setData] = useState({data: []});
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [err, setErr] = useState('');
+
+//   const handleClick = async () => {
+//     setIsLoading(true);
+//     try {
+//       const {data} = await axios.get('http://localhost:3000/tasks', {
+//         headers: {
+//           Accept: 'application/json',
+//         },
+//       });
+
+//       console.log('data is: ', JSON.stringify(data, null, 4));
+
+//       setData(data);
+//     } catch (err) {
+//       setErr(err.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   console.log(data);
+
+//   return (
+//     <div>
+//       {err && <h2>{err}</h2>}
+
+//       <button onClick={handleClick}>Fetch data</button>
+
+//       {isLoading && <h2>Loading...</h2>}
+
+//       {data.data.tasks.map(task => {
+//         return (
+//           <div key={task.id}>
+//             <h2>{task.name}</h2>
+//             <br />
+//           </div>
+//         );
+//       })}
+//       <p>{ JSON.stringify(data) }</p>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// mycomponent.js
+import React, { useEffect, useState} from 'react';
+import axios from 'axios';
+
+const MyComponent = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () =>{
+      setLoading(true);
+      try {
+        const {data: response} = await axios.get('http://localhost:3000/tasks');
+        console.log(data);
+        setData(response);
+      } catch (error) {
+        console.error(error.message);
+      }
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {loading && <div>Loading</div>}
+    {!loading && (
+      <div >
+        <h2>Tasks:</h2>
+        
+        {data.map(task => (<p key={task.id}>
+          {task.id}. {task.name}, Doses Requred: {task.doses_required}. Doses Given: {task.doses_given}</p>))}
+      </div>
+    )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default MyComponent;
