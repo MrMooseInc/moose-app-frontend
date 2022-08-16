0640ab5e7757044e1823f525340a807903222e43
greg <gregorybwest@gmail.com>
2022-08-15 17:56:55 -0700

    addDose function adds one dose, passed to components as onAddDose

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState} from 'react';
 import axios from 'axios';
 import Tasks from './Components/Tasks.js'
 import FrenchiePicture from './Components/FrenchiePicture.js'
+import Task from './Components/Task.js';
 
 function App() {
 
   const [picture, setPicture] = useState([])
   const [loading, setLoading] = useState(true)
   const [data, setData] = useState([])
+
 
   useEffect(() => {
     const fetchData = async () =>{
@@ -37,13 +39,34 @@ function App() {
     return data.message
   }
 
+const addDose = async (id) => {
+  const res = await fetch(`http://localhost:3000/tasks/${id}/add_dose`, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(Task),
+  })
+
+  const dose_data = await res.json()
+  console.log("Dose added:", dose_data.task.doses_given)
+
+  // setData([...data, dose_data])
+  setData(
+    data.map((task) =>
+      task.id === id ? { ...task, doses_given: dose_data.task.doses_given } : task
+    )
+  )
+
+}
+
  return (
     <div>
     {loading && <div>Loading</div>}
     {!loading && (
       <div >
         <h2>Tasks:</h2>
-        <Tasks data={data} />
+        <Tasks data={data} onAddDose={addDose}/>
       </div>
     )}
     <FrenchiePicture picture={picture} />
--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,10 +1,13 @@
-const Task = ({task}) => {
+const Task = ({ task, onAddDose }) => {
   return (
     <div>
       <h1>{task.name}</h1>
-      <p key={task.id}>, Doses Requred: {task.doses_required}. Doses Given: {task.doses_given}</p>
+      <p key={task.id}>
+        Doses Requred: {task.doses_required}. Doses Given: {task.doses_given}
+      </p>
+      <button onClick={() => onAddDose(task.id)}>Add Dose</button>
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
