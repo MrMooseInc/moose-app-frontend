2252352d01e9db297b85f2dc15892f3d9d083ac1
greg <gregorybwest@gmail.com>
2022-09-10 10:49:12 -0700

    add task adds new task to database and displays to user

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -2,23 +2,50 @@ import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import { useState } from "react";
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
+  const [name, setName] = useState("");
+  const [doses_required, setDoses] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if(!name || !doses_required)  {
+      alert('Please enter task name and doses required')
+      return
+    }
+
+    onAdd({ name, doses_required })
+    setName('')
+    setDoses('')
+  }
+
   return (
     <div>
       <Container>
         <Card>
           <Card.Header>Add Task</Card.Header>
           <Card.Body>
-            <Form>
+            <Form onSubmit={onSubmit}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Task Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter task name" />
+                <Form.Control
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Enter task name"
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Doses Required</Form.Label>
-                <Form.Control type="text" placeholder="Enter doses required" />
+                <Form.Control
+                  type="number"
+                  value={doses_required}
+                  onChange={(e) => setDoses(e.target.valueAsNumber)}
+                  placeholder="Enter doses required"
+                />
               </Form.Group>
               <Button variant="primary" type="submit">
                 Submit
