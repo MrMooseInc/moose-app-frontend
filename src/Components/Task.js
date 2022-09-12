a93805f8bb9943009b1f9705eb8da655325f0fbb
stefan <swebster08@gmail.com>
2022-09-12 11:09:10 -0700

    Moved Delete Button to Corner

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,11 +1,16 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { XLg } from "react-bootstrap-icons";
 
 const Task = ({ task, onAddDose, onRemoveDose, onDeleteTask }) => {
   return (
     <>
       <Card key={task.id}>
-        <Card.Header>{task.name}</Card.Header>
+        <Card.Header>
+          {task.name} 
+          <Button className="float-end" variant="outline-danger" size="sm" onClick={() => onDeleteTask(task.id)}>
+            <XLg/>
+          </Button></Card.Header>
         <Card.Body>
           <Card.Text>
             Doses Given: {task.doses_given}
@@ -15,11 +20,8 @@ const Task = ({ task, onAddDose, onRemoveDose, onDeleteTask }) => {
           <Button variant="primary" size="lg" onClick={() => onAddDose(task.id)}>
             Add Dose
           </Button>{" "}
-          <Button variant="secondary" size="xs" onClick={() => onRemoveDose(task.id)}>
+          <Button variant="secondary" size="md" onClick={() => onRemoveDose(task.id)}>
             Remove Dose
-          </Button>{" "}
-          <Button className="float-end" variant="danger" size="xs" onClick={() => onDeleteTask(task.id)}>
-            Delete Task!
           </Button>
         </Card.Body>
       </Card>
