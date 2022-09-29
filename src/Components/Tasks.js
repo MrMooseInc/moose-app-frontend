b426b63acc313090f20637d03c6e9b17e2778530
greg <gregorybwest@gmail.com>
2022-09-28 19:43:45 -0700

    adds functioning edit task button

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -2,12 +2,12 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Task from "./Task.js";
 
-const Tasks = ({ data, onAddDose, onRemoveDose, onDeleteTask }) => {
+const Tasks = ({ data, onAddDose, onRemoveDose, onDeleteTask, onEdit }) => {
   return (
     <Container className="float-none">
       {data.map((task) => (
         <>
-          <Task task={task} onAddDose={onAddDose} onRemoveDose={onRemoveDose} onDeleteTask={onDeleteTask} />
+          <Task task={task} onAddDose={onAddDose} onRemoveDose={onRemoveDose} onDeleteTask={onDeleteTask} onEdit={onEdit} />
           <br />
         </>
       ))}
